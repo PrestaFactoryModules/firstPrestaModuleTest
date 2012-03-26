7dd07d8dd93cbc0d11ac083fa12617902a285c2f
kzone <PrestaFactoryModules@gmail.com>
2012-03-26 12:37:28 +0200

    initialize file path & autoload into test files

--- Bootstrap.php
+++ Bootstrap.php
@@ -1,3 +1,19 @@
 <?php
 error_reporting(E_ALL | E_STRICT);
 
+define('CLASS_DIR','/var/www/prestashop/classes');
+define ('CONFIG_DIR','/var/www/prestashop/config');
+define('PHPUNIT_DIR','/usr/share/php/PHPUnit34/usr/share/php/PHPUnit');//version 3.4
+define('TEST_DIR','/var/www/prestashop/modules/firstPrestaModule');
+
+
+set_include_path (get_include_path().PATH_SEPARATOR . CLASS_DIR);
+set_include_path (get_include_path().PATH_SEPARATOR . CONFIG_DIR);
+set_include_path (get_include_path().PATH_SEPARATOR . PHPUNIT_DIR);
+set_include_path (get_include_path().PATH_SEPARATOR . TEST_DIR);
+/**
+ * Any other component that registers an autoloader unregisters the default __autoload().
+ *
+ */
+require_once 'autoload.php';
+spl_autoload_register('__autoload');
--- FirstPrestaModuleTest.php
+++ FirstPrestaModuleTest.php
@@ -1,8 +1,27 @@
 <?php 
+
+error_reporting(E_ALL | E_STRICT);
+
+define('CLASS_DIR','/var/www/prestashop/classes');
+define ('CONFIG_DIR','/var/www/prestashop/config');
+define('PHPUNIT_DIR','/usr/share/php/PHPUnit34/usr/share/php/PHPUnit');//version 3.4
+define('TEST_DIR','/var/www/prestashop/modules/firstPrestaModule');
+
+
+set_include_path (get_include_path().PATH_SEPARATOR . CLASS_DIR);
+set_include_path (get_include_path().PATH_SEPARATOR . CONFIG_DIR);
+set_include_path (get_include_path().PATH_SEPARATOR . PHPUNIT_DIR);
+set_include_path (get_include_path().PATH_SEPARATOR . TEST_DIR);
+/**
+ * Any other component that registers an autoloader unregisters the default __autoload().
+ *
+ */
+require_once 'autoload.php';
+spl_autoload_register('__autoload');
 // Call MyClassTest::main() if this source file is executed directly.
-if (!defined('PHPUnit_MAIN_METHOD')) {
+/* if (!defined('PHPUnit_MAIN_METHOD')) {
 	define('PHPUnit_MAIN_METHOD', 'FirstPrestaModuleTest::main');
-}
+} */
 
 /*===========================================================================*/
 
@@ -28,7 +47,7 @@ class FirstPrestaModuleTest extends PHPUnit_Framework_TestCase {
 	 * initialize your environment & framework for tests
 	 */
 	public function setUp(){
-		include_once 'Bootstrap.php';
+		//include_once 'Bootstrap.php';
 		require_once 'config.inc.php'; // nécessaire pour les variable globales 
 									   // instanciées par prestashop !!
 									   // Sans, $this->name='firstPrestaModule'; produira
@@ -44,7 +63,7 @@ Use of undefined constant _PS_MAGIC_QUOTES_GPC_ - assumed '_PS_MAGIC_QUOTES_GPC_
 										* si l'on commente l'inti de cette variable c'est OK !
 									    */
 		
-		ControllerFactory::getController('IndexController')->run();
+		//ControllerFactory::getController('IndexController')->run();
 		$this->module = new firstPrestaModule();
 	}
 	
@@ -86,6 +105,6 @@ Use of undefined constant _PS_MAGIC_QUOTES_GPC_ - assumed '_PS_MAGIC_QUOTES_GPC_
 /*=========================================================================*/
 
 // Call MyClassTest::main() if this source file is executed directly.
-if (PHPUnit_MAIN_METHOD == 'FirstPrestaModuleTest::main') {
+/* if (PHPUnit_MAIN_METHOD == 'FirstPrestaModuleTest::main') {
 	FirstPrestaModuleTest::main();
-}
+} */
